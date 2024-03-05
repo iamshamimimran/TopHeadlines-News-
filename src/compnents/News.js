31d3f181170e7ca1d62c20a6bb2d4bb7b591bda7
Shamim Imran <shamimimran35@gmail.com>
2024-03-05 12:50:18 +0530

    Nxt And Previous Added

--- src/compnents/News.js
+++ src/compnents/News.js
@@ -6,16 +6,43 @@ export default class News extends Component {
         super();
         this.state = {
             articles : [],
-            loading: false
+            loading: false,
+            page: 1
         }
     }
 
     async componentDidMount(){
-        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=98e9ffce75414c09b3936d2dcb39f79d";
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=98e9ffce75414c09b3936d2dcb39f79d&page=1&pageSize=8";
         let data = await fetch(url);
         let parseData = await data.json()
-        this.setState({articles: parseData.articles})
+        console.log("ParseData");
+        this.setState({articles: parseData.articles, totalResults:parseData.totalResults})
         
+    }
+    handlePre = async ()=>{
+      console.log("Previous");
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=98e9ffce75414c09b3936d2dcb39f79d&page=${this.state.page - 1}&pageSize=8`;
+        let data = await fetch(url);
+        let parseData = await data.json()
+        this.setState({
+          page: this.state.page - 1,
+        articles: parseData.articles
+        })
+    }
+    handleNxt = async ()=>{
+      console.log("Next");
+      if(Math.ceil(this.state.page + 1>this.state.totalResults/9)){
+
+      }
+      else{
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=98e9ffce75414c09b3936d2dcb39f79d&page=${this.state.page + 1}&pageSize=8`;
+        let data = await fetch(url);
+        let parseData = await data.json()
+        this.setState({
+          page:this.state.page + 1,
+          articles: parseData.articles
+        })
+      }
     }
 
   render() {
@@ -28,9 +55,9 @@ export default class News extends Component {
           </div>
         })}
       </div>
-      <div className='d-flex justify-content-between'>
-  <button type="button" className="btn btn-dark">&larr; Previous</button>
-  <button type="button" className="btn btn-dark">Next &rarr;</button>
+      <div className='d-flex justify-content-between my-2'>
+  <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePre}>&larr; Previous</button>
+  <button type="button" className="btn btn-dark" onClick={this.handleNxt}>Next &rarr;</button>
   </div>
         </div>
 
